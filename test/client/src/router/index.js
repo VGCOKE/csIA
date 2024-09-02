eabb7296a560a96ec5bfc9f81830cf770014f4b0
Human666 <cokechicken3@gmail.com>
2024-09-02 22:47:42 +0800

    Initial commit

---
+++ test/client/src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Ping from '../components/Ping.vue'
+import Books from '../components/Books.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/ping',
+      name: 'ping',
+      component: Ping
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      //component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/',
+      name: 'Books',
+      component: Books,
+    }
+  ]
+})
+
+export default router
