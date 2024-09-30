88a0f3a56c507ed5fed97451a7fa96d8876b330c
Human666 <cokechicken3@gmail.com>
2024-09-30 15:37:58 +0800

    30/9

--- csIA/django/RestaurantApp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework.response import Response 
-from rest_framework.decorators import api_view
-
-# Create your views here.
-@api_view(['GET'])
-def greeting_message(request):
-    return Response({"message":"Hello from Django backend..."})
